640bb2f45882506f966a54d35ce6efbff0d20526
ooinl77 <ooinl77@naver.com>
2025-06-12 15:43:56 +0900

    Fix: 퀘스트 스케쥴러 새벽 5시로 변경

--- src/main/java/com/popoworld/backend/diary/child/dto/DiaryTodayRequest.java
+++ src/main/java/com/popoworld/backend/diary/child/dto/DiaryTodayRequest.java
@@ -12,7 +12,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DiaryTodayRequest {
-//    private String accessToken;
     private Emotion emotion;
     private String description;
 }
