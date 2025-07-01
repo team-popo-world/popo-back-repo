15f9a1938f5bfeb8fe5920e21b03f44086d3713b
haewonee <godnjs5870@naver.com>
2025-07-01 15:50:04 +0900

    Fix: ML 호출 url 변경

--- src/main/java/com/popoworld/backend/market/analytics/config/MarketAnalyticsConfig.java
+++ src/main/java/com/popoworld/backend/market/analytics/config/MarketAnalyticsConfig.java
@@ -10,7 +10,7 @@ public class MarketAnalyticsConfig {
     @Bean
     public WebClient purchaseWebClient(){
         return WebClient.builder()
-                .baseUrl("http://43.203.175.69:8001/")
+                .baseUrl("http://15.164.235.203:8001/")
                 .codecs(configurer->configurer.defaultCodecs().maxInMemorySize(1024*1024))
                 .build(); //한 요청당 응답 데이터를 최대 1MB까지 메모리 올릴 수 있게 설정
     }
